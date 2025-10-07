0dd30ec898e7ab3752e17a94a0c245c50edd8cef
Vadym Yemets <v.yemets@levi9.com>
2025-10-07 14:00:31 +0300

    update Course controller, course list

--- force-app/main/default/lwc/courseList/courseList.js
+++ force-app/main/default/lwc/courseList/courseList.js
@@ -1,7 +1,8 @@
 import { LightningElement, track, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import getCourses from '@salesforce/apex/CourseController.getCourses';
 
-export default class CourseList extends LightningElement {
+export default class CourseList extends NavigationMixin(LightningElement) {
     @track courses;
     @track error;
 
@@ -14,5 +15,17 @@ export default class CourseList extends LightningElement {
             this.error = error;
             this.courses = undefined;
         }
+    }
+
+    handleNavigate(event) {
+        const recordId = event.target.dataset.id;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: recordId,
+                objectApiName: 'Course__c',
+                actionName: 'view'
+            }
+        });
     }
 }
